56fe9853c6103bac3e7bc53fd91b18679af001d0
rhein8th <138426399+rhein8th@users.noreply.github.com>
2025-02-15 20:10:20 +0800

    Initial commit in Product List page
    Change file names

---
+++ cypress/e2e/pages/ProductListPage.js
@@ -0,0 +1,11 @@
+class Products
+    {
+
+    logo() { return cy.get(".app_logo"); }
+    cartBtn() { return cy.get(".shopping_cart_link"); }
+    pageTitle() { return cy.get(".title"); }
+
+ 
+ 
+    }
+    export default Products;
