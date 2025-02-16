c5a5346fe647fc0f13713f4474cc08c8bf9a2a8a
rhein8th <138426399+rhein8th@users.noreply.github.com>
2025-02-16 15:58:41 +0800

    Added Sort Functionality Validation in Product List page

--- cypress/e2e/pages/ProductListPage.js
+++ cypress/e2e/pages/ProductListPage.js
@@ -4,8 +4,14 @@ class Products
     logo() { return cy.get(".app_logo"); }
     cartBtn() { return cy.get(".shopping_cart_link"); }
     pageTitle() { return cy.get(".title"); }
-
- 
- 
+    sortBtn() { return cy.get(".product_sort_container"); }
+    invetoryList() { return cy.get(".inventory_list"); }
+    productCtnr() { return cy.get(".inventory_item"); }
+    productImg() { return cy.get(".inventory_item > .inventory_item_img"); }
+    productName() { return cy.get(".inventory_item_name"); }
+    productDesc() { return cy.get(".inventory_item_desc"); }
+    productPrice() { return cy.get(".inventory_item_price"); }
+    addToCartBtn() { return cy.get("button.btn_inventory"); }
+    
     }
     export default Products;
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -98,13 +98,16 @@ Cypress.Commands.add("validateFooter", () => {
     });
 });
 
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//Adding product to cart
+Cypress.Commands.add("selectProduct", (productName) => { 
+
+    cy.get("h4.card-title").each(($el, index, $list) => {
+        if ($el.text().includes(productName))
+        {
+         cy.get("button.btn.btn-info").eq(index).click()
+    
+        }
+    })
+
+     
+ })
