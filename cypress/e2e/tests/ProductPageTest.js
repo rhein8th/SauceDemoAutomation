d7293411dd31f6057f546fe1fb43b74170c7d5f9
rhein8th <138426399+rhein8th@users.noreply.github.com>
2025-02-17 22:40:14 +0800

    Added proceeding to products page and validating details

--- cypress/e2e/tests/ProductPageTest.js
+++ cypress/e2e/tests/ProductPageTest.js
@@ -1,26 +1,62 @@
 /// <reference types="cypress" />
-import ProductPage from "../pages/ProductListPage";
-import ProductListPage from "../pages/ProductPage";
+import ProductPage from "../pages/ProductPage";
+import ProductListPage from "../pages/ProductListPage";
 import CartNavigation from "../sharedComponents/CartNavigation";
 
-    const product = new ProductListPage(); 
-    const productList = new ProductListPage();
-    const cart = new CartNavigation();
+const product = new ProductPage(); 
+const productList = new ProductListPage();
+const cart = new CartNavigation();
 
-before(()=>{
+before(() => {
     cy.clearAppData();
-})
+});
 
 beforeEach(() => {
     cy.login();
     // cy.visit("/inventory"); 
 });
 
-describe("Product Page Test Suite", ()=>{
+describe("Product Page Test Suite", () => {
 
-    it("Validate Product Page",()=>{ //validate page elements and should be equal to what was clicked (name, desc, price)
-
-        cy.contains("Sauce Labs Backpack").click();
+    it("Validate Product Page", () => {
+        cy.fixture("product.json").then((productData) => {
+            const targetProduct = productData.productName;
+            productList.productCtnr().should("be.visible");
+    
+            productList.productName().each(($el, index) => {
+                if ($el.text() == targetProduct) {
+                    productList.productName().eq(index).then(($nameElement) => { //get name
+                        const expectedName = $nameElement.text();
+    
+                        productList.productDesc().eq(index).then(($descElement) => { //get desc
+                            const expectedDesc = $descElement.text();
+    
+                            productList.productPrice().eq(index).then(($priceElement) => { //get price
+                                const expectedPrice = $priceElement.text();
+    
+                                productList.productName().eq(index).click(); //click the product that match
+                                product.productCtnr().should("be.visible");
+    
+                                product.productName().then(($productPageNameElement) => { //validation name
+                                    const actualName = $productPageNameElement.text();
+                                    expect(actualName).to.equal(expectedName);
+                                });
+    
+                                product.productDesc().then(($productPageDescElement) => { //validation desc
+                                    const actualDesc = $productPageDescElement.text();
+                                    expect(actualDesc).to.equal(expectedDesc);
+                                });
+    
+                                product.productPrice().then(($productPagePriceElement) => { //validation price
+                                    const actualPrice = $productPagePriceElement.text();
+                                    expect(actualPrice).to.equal(expectedPrice);
+                                });
+                            });
+                        });
+                    }); 
+                } 
+            }); 
+        }); 
         cy.validateHamburgerMenu();
         cy.validateCartButton();
         cy.validateFooter()
@@ -32,16 +68,11 @@ describe("Product Page Test Suite", ()=>{
         product.productName().should("be.visible");
         product.productDesc().should("be.visible");
         product.productPrice().should("be.visible");
-        product.addToCartBtn().should("be.visible");
+        product.addToCartBtn().should("be.visible")
+    }); 
+  
 
-
-    })
-    
     //validate adding to cart & cart button text
     //validate removing to cart & cart button text
     //validate back button
- 
-       
 });
-
-   
