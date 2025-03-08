4bea0ed39ececc3d7b46236edc1a4c5a374e5605
rhein8th <138426399+rhein8th@users.noreply.github.com>
2025-03-08 14:24:49 +0800

    Added Checkout page validation

---
+++ cypress/e2e/tests/CheckoutTest.js
@@ -0,0 +1,68 @@
+/// <reference types="cypress" />
+import ProductPage from "../pages/ProductPage";
+import ProductListPage from "../pages/ProductListPage";
+import CartPage from "../pages/CartPage";
+import CheckoutPage from "../pages/CheckoutPage";
+
+const product = new ProductPage(); 
+const productList = new ProductListPage();
+const cartPage = new CartPage();
+const checkoutPage = new CheckoutPage();
+
+before(() => {
+    cy.clearAppData();
+});
+
+beforeEach(() => {
+    cy.login();
+});
+
+describe("Checkout Page Test Suite", () => {
+
+    it("Validate Checkout page", () => {
+        cy.validateCheckoutpage();
+    });
+
+    it("Validate Text Fields", () => {
+        cy.fixture("users").then((users) => {
+            const fName = users.userInfo.firstName;
+            const lName = users.userInfo.lastName;
+           
+            cy.validateCheckoutpage();
+            checkoutPage.continueBtn().click();
+            checkoutPage.fNameInput().should("have.class", "form_input error");
+            checkoutPage.lNameInput().should("have.class", "form_input error");
+            checkoutPage.zipCodeInput().should("have.class", "form_input error");
+            checkoutPage.errorMsg().should("be.visible").should("have.text", "Error: First Name is required");
+
+
+            checkoutPage.fNameInput().type(fName);
+            checkoutPage.continueBtn().click();
+            checkoutPage.errorMsg().should("be.visible").should("have.text", "Error: Last Name is required");
+
+            checkoutPage.lNameInput().type(lName);
+            checkoutPage.continueBtn().click();
+            checkoutPage.errorMsg().should("be.visible").should("have.text", "Error: Postal Code is required");
+        });
+     })
+
+
+
+     it.only("Validate Proceeding to Confirm Checkout page", () => {
+        cy.fixture("users").then((users) => {
+            const fName = users.userInfo.firstName;
+            const lName = users.userInfo.lastName;
+            const zCode = users.userInfo.zipCode;
+  
+            cy.validateCheckoutpage();
+
+            checkoutPage.fNameInput().type(fName);
+            checkoutPage.lNameInput().type(lName);
+            checkoutPage.zipCodeInput().type(zCode);
+
+            checkoutPage.continueBtn().click();
+            cy.url().should("include","/checkout-step-two");
+        });
+     })
+    
+});
--- cypress/e2e/tests/ProductListTest.js
+++ cypress/e2e/tests/ProductListTest.js
@@ -20,7 +20,7 @@ describe("Product List Page Test Suite", ()=>{
     it("Validate Product List Page",()=>{
         cy.validateHamburgerMenu();
         cy.validateCartButton().go("back");
-        cy.validateFooter()
+        cy.validateFooter();
 
         productList.logo().should("be.visible");
         productList.pageTitle().should("be.visible");
