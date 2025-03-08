5b2392ec1a992fa810a121c39a296e21e7380586
rhein8th <138426399+rhein8th@users.noreply.github.com>
2025-03-09 00:18:47 +0800

    Updated Confirm Checkout page Validations

--- cypress/e2e/pages/ConfirmCheckoutPage.js
+++ cypress/e2e/pages/ConfirmCheckoutPage.js
@@ -1,21 +1,15 @@
 class Checkout
     {
-
     logo() { return cy.get(".app_logo"); }
-
     pageTitle() { return cy.get(".title"); }
     infoCntr() { return cy.get(".checkout_info"); }
-  
     qtyLbl() { return cy.get(".cart_quantity_label"); }
     descLbl() { return cy.get(".cart_desc_label"); }
-    
     productCtnr() { return cy.get(".cart_item"); }
     productQty() { return cy.get(".cart_quantity"); }
-
     productName() { return cy.get(".inventory_item_name"); }
     productDesc() { return cy.get(".inventory_item_desc"); }
     productPrice() { return cy.get(".inventory_item_price"); }
-
     summaryInfo() { return cy.get(".summary_info"); }
     paymentInfo() { return cy.get("div[data-test='payment-info-label']"); }
     paymentVal() { return cy.get("div[data-test='payment-info-value']"); }
@@ -25,13 +19,7 @@ class Checkout
     subTotal() { return cy.get("div[data-test='subtotal-label']"); }
     tax() { return cy.get("div[data-test='tax-label']"); } //tax 1.0801
     totalPrice() { return cy.get("div[data-test='total-label']"); }
-
-    
-
     cancelBtn() { return cy.get("#cancel"); }
-    continueBtn() { return cy.get("#continue"); }
-    
-    errorMsg() { return cy.get("h3"); }
-
+    finishBtn() { return cy.get("#finish"); }
     }
-    export default Checkout;
+export default Checkout;
